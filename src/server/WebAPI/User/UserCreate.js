7ca772269977f68c79eeb601f052c42b9ea40db0
wanli_cheng <wanli_cheng@labif.com>
2017-11-08 13:49:07 +0800

    merge master

--- src/server/WebAPI/User/UserCreate.js
+++ src/server/WebAPI/User/UserCreate.js
@@ -10,14 +10,36 @@ var UserCreateHandler = function(){};
 _.extend(UserCreateHandler.prototype,RequestHandlerBase.prototype);
 
 
-
+/**
+     * @api {post} /user/cerate 新增用户
+     * @apiGroup User
+     * @apiDescription 新增用户
+     * @apiParam {String} email 用户邮箱
+     * @apiParam {String} password 用户密码
+     * @apiParam {String} phone 用户手机号
+     * @apiError 1000001 用户邮箱是必填项
+     * @apiError 1000002 用户昵称是必填项
+     * @apiError 1000003 用户密码是必填项
+     * @apiError 1000004 用户手机号是必填项
+     * @apiError 1000005 用户已经存在
+     * @apiError 1000006 邮箱格式不合法
+     * @apiError 1000007 手机号格式不合法
+     * @apiError 1000008 密码长度不能小于6位
+     * @apiSuccessExample Success-Response:
+    { code: 1,
+  	  data: 
+   		{ 
+   			nickname:'paimu' 
+        }
+     }
+    */
 
 UserCreateHandler.prototype.attach = function(router){
 	var self = this;
 	router.post('/',function(request,response){
 		self.validate(request.body,function(err,user){
 			if(!err){
-				self.successResponse(response,Const.responsecodeSucceed,{user:user});  
+				self.successResponse(response,Const.responsecodeSucceed,{nickname:user.nickname}); 
 			}else{
 				self.successResponse(response,err);  
 			}
@@ -25,26 +47,51 @@ UserCreateHandler.prototype.attach = function(router){
 	});
 };
 
-UserCreateHandler.prototype.validate = function(requestBody,callback){
-	if(_.isEmpty(requestBody.email)){
+UserCreateHandler.prototype.validate = function(params,callback){
+	var emailRegx = /^(\w)+(\.\w+)*@(\w)+((\.\w+)+)$/;
+	var phoneRegx = /^(13[0-9]|15[012356789]|17[013678]|18[0-9]|14[57])[0-9]{8}$/;
+	if(_.isEmpty(params.email)){
+		console.log("err","no email");
 		callback(Const.resCodeUserCreateNoEmail);
-	}else if(_.isEmpty(requestBody.nickname)){
+		return;
+	}else if(_.isEmpty(params.nickname)){
+		console.log("err","no nickname");
 		callback(Const.resCodeUserCreateNoNickName);
-	}else if(_.isEmpty(requestBody.password)){
+		return;
+	}else if(_.isEmpty(params.password)){
+		console.log("err","no password");
 		callback(Const.resCodeUserCreateNoPassword);
-	}else if(_.isEmpty(requestBody.phone)){
+		return;
+	}else if(_.isEmpty(params.phone)){
+		console.log("err","no phone");
 		callback(Const.resCodeUserCreateNoPhone);
+		return;
+	}else if(!emailRegx.test(params.email)){
+		console.log("err","email is valid");
+		callback(Const.resCodeUserCreateEmailIsValid);
+		return;
+	}else if(!phoneRegx.test(params.phone)){
+		console.log("err","phone is valid");
+		callback(Const.resCodeUserCreatePhoneIsValid);
+		return;
+	}else if(params.password.length<6){
+		console.log("err","password's length is less than 6");
+		callback(Const.resCodeUserCreatePasswordLengthIsValid);
+		return;
 	}
 
 	var userModel = UserModel.get();
 	userModel.findOne({email:requestBody.email},function(err,user){
-		if(!_.isNull(user)){
-			callback(null,user);
+		if(_.isNull(user)){
+			user.isAccountEnabled = 0;
+			user.save(function(err){
+				callback(null,user);
+			})
 		}else{
-			callback(Const.resCodeNullUser,null);
+			callback(Const.resCodeUserCreateExistUser);
 		}
 	});
 };
 
-new TestHandler().attach(router);
+new UserCreateHandler().attach(router);
 module["exports"] = router;
--- src/server/lib/consts.js
+++ src/server/lib/consts.js
@@ -8,11 +8,14 @@
      Const.httpCodeServerError =500;
      Const.httpCodeForbidden = 403;
 
-     Const.resCodeNullUser = 1000000;
      Const.resCodeUserCreateNoEmail = 1000001;
      Const.resCodeUserCreateNoNickName = 1000002;
      Const.resCodeUserCreateNoPassword = 1000003;
      Const.resCodeUserCreateNoPhone = 1000004;
+     Const.resCodeUserCreateExsitUser = 1000005;
+     Const.resCodeUserCreateEmailIsValid = 1000006;
+     Const.resCodeUserCreatePhoneIsValid = 1000007;
+     Const.resCodeUserCreatePasswordLengthIsValid = 1000008;
 
 
      // Exports ----------------------------------------------
