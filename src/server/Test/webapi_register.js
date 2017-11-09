efae53402c9a888f9de4657d2018f2ff56c533fc
wanli_cheng <wanli_cheng@labif.com>
2017-11-09 14:09:38 +0800

    merge master

--- src/server/Test/webapi_register.js
+++ src/server/Test/webapi_register.js
@@ -2,7 +2,7 @@ var should = require('should');
 var request = require('supertest');
 var app = require('../mainTest');
 var helper = require('./helper');
-
+var Const = require('../lib/consts');
 describe('WEB', function () {
 
     var req, res;
@@ -21,13 +21,98 @@ describe('WEB', function () {
     			if (err) {
     				throw err;
     			}
-			
-                console.log(res.body)
                 
+                console.log(res.body)
+                res.body.code.should.be.equal(1);
                 done();
             
             });   
             
         });
+
+        it('if email is null or empty', function (done) {
+            var body = {                            
+                            email: "",
+                            password:"rex123"                         
+                        };  
+            
+            request(app)
+                .post('/dogsystem/v1/user/register')
+                .send(body)
+                .end(function (err, res) {
+
+    			if (err) {
+    				throw err;
+    			}
+                res.body.code.should.be.equal(Const.resCodeRegisterNoEmail);
+                done();
+            
+            });   
+            
+        });
+
+        it('if password is null or empty', function (done) {
+            var body = {                            
+                            email: "test@test" + global.getRandomStr() + "2.com",
+                            password:""                         
+                        };  
+            
+            request(app)
+                .post('/dogsystem/v1/user/register')
+                .send(body)
+                .end(function (err, res) {
+
+    			if (err) {
+    				throw err;
+    			}
+                res.body.code.should.be.equal(Const.resCodeRegisterNoPassword);
+                done();
+            
+            });   
+            
+        });
+
+        it('if password is less than min length', function (done) {
+            var body = {                            
+                            email: "test@test" + global.getRandomStr() + "2.com",
+                            password:"rex1"                         
+                        };  
+            
+            request(app)
+                .post('/dogsystem/v1/user/register')
+                .send(body)
+                .end(function (err, res) {
+
+    			if (err) {
+    				throw err;
+    			}
+                res.body.code.should.be.equal(Const.resCodeSetPassWordLengthError);
+                done();
+            
+            });   
+            
+        });
+        //test@testCIyfp2.com
+        it('if password is less than min length', function (done) {
+            var body = {                            
+                            email: "test@testCIyfp2.com",
+                            password:"rex123456"                         
+                        };  
+            
+            request(app)
+                .post('/dogsystem/v1/user/register')
+                .send(body)
+                .end(function (err, res) {
+
+    			if (err) {
+    				throw err;
+    			}
+                res.body.code.should.be.equal(Const.resCodeSetPasswordWrongPhone);
+                done();
+            
+            });   
+            
+        });
+
   });
     
