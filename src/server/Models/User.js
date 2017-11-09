dc058dd9b10b557fcb8dce9a5fd2579f42a7237e
wanli_cheng <wanli_cheng@labif.com>
2017-11-09 11:43:06 +0800

    merge master

--- src/server/Models/User.js
+++ src/server/Models/User.js
@@ -7,27 +7,27 @@ const Utils = require("../lib/Utils");
 var async = require('async');
 const Const = require("../lib/consts");
 
-var UserModel  = function(){};
+var UserModel = function () { };
 
 
-UserModel.prototype.init = function(mongoose){
+UserModel.prototype.init = function (mongoose) {
 	this.schema = new mongoose.Schema({
-			email:{type:String, index:true,unique: true}, 			
-			nickname: String,
-			password: String,			
-			phone:{type:String, index:true},  //可以为空
-			isAccountEnabled:Number, //账号状态： 0：未激活  1.激活。  -1：冻结
-			created:Date, //注册时间
-			activated:Date, //激活时间
-			token: {type:String, index:true,unique: true},
-			// tokenDate:Number,//token有效期
-		    organization: { type: mongoose.Schema.Types.ObjectId, ref: Config.dbCollectionPrefix + "organizations" },  //账号所属机构
-		    //安全信息
-		    safetyInformation:{
-		    	 ip:String,
-		    	 browserType:String, //游览器类型
-		    	 deviceType:String, //设备信息
-		    }
+		email: { type: String, index: true, unique: true },
+		nickname: String,
+		password: String,
+		phone: { type: String, index: true },  //可以为空
+		isAccountEnabled: Number, //账号状态： 0：未激活  1.激活。  -1：冻结
+		created: Date, //注册时间
+		activated: Date, //激活时间
+		token: { type: String, index: true, unique: true },
+		// tokenDate:Number,//token有效期
+		organization: { type: mongoose.Schema.Types.ObjectId, ref: Config.dbCollectionPrefix + "organizations" },  //账号所属机构
+		//安全信息
+		safetyInformation: {
+			ip: String,
+			browserType: String, //游览器类型
+			deviceType: String, //设备信息
+		}
 	});
 
 	this.model = mongoose.model(Config.dbCollectionPrefix + "users", this.schema);
@@ -35,8 +35,8 @@ UserModel.prototype.init = function(mongoose){
 
 
 
-UserModel.get = function(){
-    return DatabaseManager.getModel('User').model;    
+UserModel.get = function () {
+	return DatabaseManager.getModel('User').model;
 }
 
 
--- src/server/lib/Utils.js
+++ src/server/lib/Utils.js
@@ -1,24 +1,24 @@
 var moment = require('moment');
 
-    // Class ------------------------------------------------
-    function Utils() {
-    };
+// Class ------------------------------------------------
+function Utils() {
+};
 
 
- 	Utils.prototype.now = now;
-
-
-
+Utils.prototype.now = now;
 
 
 
- 	 function now(){
-      
-		var time =moment(new Date()).local();;
-    
-		return time;
-
-    }
 
 
-   module["exports"] = new Utils();
+
+function now() {
+
+  var time = moment(new Date()).local();;
+
+  return time;
+
+}
+
+
+module["exports"] = new Utils();
