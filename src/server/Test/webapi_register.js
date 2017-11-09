70ef098e03b04918f7a49b2301268aaf3e5c94ba
wanli_cheng <wanli_cheng@labif.com>
2017-11-09 14:29:23 +0800

    merge master

--- src/server/Test/webapi_register.js
+++ src/server/Test/webapi_register.js
@@ -7,111 +7,110 @@ describe('WEB', function () {
 
     var req, res;
 
-        it('should be register Sucessful', function (done) {
-            var body = {                            
-                            email: "test@test" + global.getRandomStr() + "2.com",
-                            password:"rex123"                         
-                        };  
+    it('should be register Sucessful', function (done) {
+        var body = {
+            email: "test@test" + global.getRandomStr() + "2.com",
+            password: "rex123"
+        };
 
-            request(app)
-                .post('/dogsystem/v1/user/register')
-                .send(body)
-                .end(function (err, res) {
+        request(app)
+            .post('/dogsystem/v1/user/register')
+            .send(body)
+            .end(function (err, res) {
 
-    			if (err) {
-    				throw err;
-    			}
-                
+                if (err) {
+                    throw err;
+                }
+
                 console.log(res.body)
                 res.body.code.should.be.equal(1);
                 done();
-            
-            });   
-            
-        });
 
-        it('if email is null or empty', function (done) {
-            var body = {                            
-                            email: "",
-                            password:"rex123"                         
-                        };  
-            
-            request(app)
-                .post('/dogsystem/v1/user/register')
-                .send(body)
-                .end(function (err, res) {
+            });
 
-    			if (err) {
-    				throw err;
-    			}
+    });
+
+    it('if email is null or empty', function (done) {
+        var body = {
+            email: "",
+            password: "rex123"
+        };
+
+        request(app)
+            .post('/dogsystem/v1/user/register')
+            .send(body)
+            .end(function (err, res) {
+
+                if (err) {
+                    throw err;
+                }
                 res.body.code.should.be.equal(Const.resCodeRegisterNoEmail);
                 done();
-            
-            });   
-            
-        });
 
-        it('if password is null or empty', function (done) {
-            var body = {                            
-                            email: "test@test" + global.getRandomStr() + "2.com",
-                            password:""                         
-                        };  
-            
-            request(app)
-                .post('/dogsystem/v1/user/register')
-                .send(body)
-                .end(function (err, res) {
+            });
 
-    			if (err) {
-    				throw err;
-    			}
+    });
+
+    it('if password is null or empty', function (done) {
+        var body = {
+            email: "test@test" + global.getRandomStr() + "2.com",
+            password: ""
+        };
+
+        request(app)
+            .post('/dogsystem/v1/user/register')
+            .send(body)
+            .end(function (err, res) {
+
+                if (err) {
+                    throw err;
+                }
                 res.body.code.should.be.equal(Const.resCodeRegisterNoPassword);
                 done();
-            
-            });   
-            
-        });
 
-        it('if password is less than min length', function (done) {
-            var body = {                            
-                            email: "test@test" + global.getRandomStr() + "2.com",
-                            password:"rex1"                         
-                        };  
-            
-            request(app)
-                .post('/dogsystem/v1/user/register')
-                .send(body)
-                .end(function (err, res) {
+            });
 
-    			if (err) {
-    				throw err;
-    			}
+    });
+
+    it('if password is less than min length', function (done) {
+        var body = {
+            email: "test@test" + global.getRandomStr() + "2.com",
+            password: "rex1"
+        };
+
+        request(app)
+            .post('/dogsystem/v1/user/register')
+            .send(body)
+            .end(function (err, res) {
+
+                if (err) {
+                    throw err;
+                }
                 res.body.code.should.be.equal(Const.resCodeSetPassWordLengthError);
                 done();
-            
-            });   
-            
-        });
-        //test@testCIyfp2.com
-        it('if user is registed', function (done) {
-            var body = {                            
-                            email: "test@testCIyfp2.com",
-                            password:"rex123456"                         
-                        };  
-            
-            request(app)
-                .post('/dogsystem/v1/user/register')
-                .send(body)
-                .end(function (err, res) {
 
-    			if (err) {
-    				throw err;
-    			}
-                res.body.code.should.be.equal(Const.resCodeSetPasswordWrongPhone);
+            });
+
+    });
+    //test@testCIyfp2.com
+    it('if user is registed', function (done) {
+        var body = {
+            email: "test@testCIyfp2.com",
+            password: "rex123456"
+        };
+
+        request(app)
+            .post('/dogsystem/v1/user/register')
+            .send(body)
+            .end(function (err, res) {
+
+                if (err) {
+                    throw err;
+                }
+                res.body.code.should.be.equal(Const.resCodeRegisterWrongEmail);
                 done();
-            
-            });   
-            
-        });
-  });
-    
+
+            });
+
+    });
+});
