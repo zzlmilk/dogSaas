29a0841ef88d044ec5ea47c483312dba69cc2aff
lvjisong <jsonlv@aliyun.com>
2017-11-22 09:47:22 +0800

    add dog样式调整

---
+++ src/client/js/Views/Nav/LoginAfter/LoginAfterView.js
@@ -0,0 +1,38 @@
+var Backbone = require('backbone');
+var _ = require('lodash');
+
+var Utils = require('../../../lib/utils');
+var Const = require('../../../lib/consts');
+var Config = require('../../../lib/init');
+
+// load template
+var template = require('./LoginAfter.hbs');
+
+var LoginAfterView = Backbone.View.extend({
+
+    initialize: function(options) {
+        this.render();
+    },
+    
+    render: function() {	
+
+        $(Config.defaultContaier).html(template({
+        	   
+        }));
+
+        this.onLoad();
+
+        return this;
+
+    },
+
+    onLoad: function(){
+
+        var self = this;  
+       
+
+    }
+
+});
+
+module.exports = LoginAfterView;
