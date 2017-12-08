c7aa14a567c0f32265ce6c840da6f5801ffe2c6c
chaoos <770111598@qq.com>
2017-12-08 13:34:20 +0800

    制卡数据回填

--- src/client/js/Views/Main/Dog/DogCard/CardInfo/CardInfoView.js
+++ src/client/js/Views/Main/Dog/DogCard/CardInfo/CardInfoView.js
@@ -9,23 +9,35 @@ var _ = require('lodash');
 var Utils = require('../../../../../lib/utils');
 var Const = require('../../../../../lib/consts');
 var Config = require('../../../../../lib/init');
+var CityJson = require('../../../../Parts/selectPlugin/CityJson.js');
 
 // load template
 var template = require('./CardInfo.hbs');
 
 var CardInfoView = Backbone.View.extend({
-
+    self:null,
     el: null,
     dogLicense: null,
+    districtText:null,
     initialize: function (options) {
+        self=this;
         this.el = options.el;
+
+        $.each(CityJson,
+            function (i, val) {
+                if (val.item_code == options.dogLicense.vaccineCard.info.district) {
+                    options.dogLicense.vaccineCard.info.district = val.item_name;
+                }
+            });
         this.dogLicense = options.dogLicense;
         this.render();
     },
 
     render: function () {
+
         $(this.el).html(template({
-            dogLicense:this.dogLicense
+            dogLicense: this.dogLicense,
+            districtText: this.districtText
         }));
 
         this.onLoad();
