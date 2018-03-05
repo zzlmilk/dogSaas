d07feb03f0135a14ff896084fd28f78fee77aa9a
zhanweixue <zhanwx@pet-more.cn>
2018-03-05 13:55:01 +0800

    小程序appid和secret

--- src/server/lib/init.js
+++ src/server/lib/init.js
@@ -18,8 +18,8 @@
     }
     Config.Wx={
         grant_type:'client_credential',
-        appid:'wxeb1f9b8b9c147e83',
-        secret:'7fa185f001fc0c286f8da3dce48b8d63'
+        appid:'wx40d38f59e601216f',
+        secret:'10efb790e0ecca9f117dc7c130c34ec0'
     }
   
 
