cb2f63f587694f8692de46bd065e9ccbe2c7263e
zhou zhiling <zhouzhiling@zhoudeMacBook-Air.local>
2018-03-06 17:03:00 +0800

    language.js

---
+++ src/client/js/lib/viewHelpers.js
@@ -0,0 +1,29 @@
+var CONST = require('./consts');
+var _ = require('lodash');
+var U = require('./utils.js');
+var LocalizationManager = require('./localizationManager.js');
+var Handlebars = require('hbsfy/runtime');
+
+
+
+function ViewHelpers() {
+    };
+
+
+     // Header -----------------------------------------------
+    ViewHelpers.prototype.attach = attach; 
+
+
+
+    function attach(){
+
+    	 Handlebars.registerHelper("l10n", function(text) {
+          return  LocalizationManager.localize(text);
+        });
+
+    	
+    
+    }
+
+     // Exports ----------------------------------------------
+    module["exports"] = new ViewHelpers();
