b74106634a3a7bee26db293f300d5eb2a7e7c382
lvjisong <jsonlv@aliyun.com>
2018-03-23 14:59:30 +0800

    Bug Fix

--- src/client/js/Views/HomePage/HomeMain/HomeMainView.js
+++ src/client/js/Views/HomePage/HomeMain/HomeMainView.js
@@ -50,7 +50,7 @@ var HomeMainView = Backbone.View.extend({
                 $(this).children("div").addClass("arrow").parent("li").siblings("li").children("div").removeClass("arrow");
             })
         });
-        //首页产品优势
+        //首页产品优势--PC端
         $(".tips").each(function(){
             $(this).mouseover(function () {
                 $(this).stop().animate({top:"0px"},"slow");
@@ -59,6 +59,40 @@ var HomeMainView = Backbone.View.extend({
                 $(this).stop().animate({top:"300px"});
             });
         });
+        //首页产品优势--移动端
+        function urlredirect() {
+            var sUserAgent = navigator.userAgent.toLowerCase();
+            //首先判断浏览器类型是否为移动端
+            if ((sUserAgent.match(/(ipod|iphone os|midp|ucweb|android|windows ce|windows mobile)/i))) {
+                //是移动端浏览器的话遍历蒙层元素
+                $(".tips").each(function(){
+                    //设置蒙层点击事件
+                    $(this).on('touchend', function (event) {
+                        //判断蒙层是否处于最高处
+                        if($(this).position().top==0){
+                            //处于最高处的话让蒙层回到原处
+                            $(this).stop().animate({top:"160px"});
+                        }else{
+                            //否则判断是否处于动画状态
+                            if($(this).not(":animated")){
+                                //没有处于动画状态时候，让蒙层执行动画到顶部并且让兄弟蒙层回到原处
+                                  $(this).stop().animate({top:"0px"}).parent(".col-md-4").siblings(".col-md-4").children(".tips").stop().animate({top:"160px"});
+                            }else{
+                                //处于动画状态的话即蒙层处于最高处，此时让蒙层执行动画回到原处
+                                  $(this).stop().animate({top:"160px"});
+                            }
+                        }
+                        //组织默认行为
+                        event.preventDefault();
+                    })
+                });
+            }
+        }
+        urlredirect();
+
+
+
+
         //首页小轮播图
         var mySwiperSecond = new Swiper($('.swiper-container')[1], {
             direction: 'horizontal',
