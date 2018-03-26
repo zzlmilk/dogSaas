93e3ea290b7db7638f4f66e8b8936cfa3dde678a
lvjisong <jsonlv@aliyun.com>
2018-03-26 18:07:14 +0800

    Bug Fix

--- src/client/js/Views/HomePage/Product/ProductView.js
+++ src/client/js/Views/HomePage/Product/ProductView.js
@@ -37,6 +37,38 @@ var ProductView = Backbone.View.extend({
             });
         });
 
+        //产品优势--移动端
+        function urlredirect() {
+            var sUserAgent = navigator.userAgent.toLowerCase();
+            //首先判断浏览器类型是否为移动端
+            if ((sUserAgent.match(/(ipod|iphone os|midp|ucweb|android|windows ce|windows mobile)/i))) {
+                //是移动端浏览器的话遍历蒙层元素
+                $(".tips").each(function(){
+                    //设置蒙层点击事件
+                    $(this).on('touchend', function (event) {
+                        //判断蒙层是否处于最高处
+                        if($(this).position().top==0){
+                            //处于最高处的话让蒙层回到原处
+                            $(this).stop().animate({top:"160px"});
+                        }else{
+                            //否则判断是否处于动画状态
+                            if($(this).not(":animated")){
+                                //没有处于动画状态时候，让蒙层执行动画到顶部并且让兄弟蒙层回到原处
+                                $(this).stop().animate({top:"0px"}).parent(".col-md-4").siblings(".col-md-4").children(".tips").stop().animate({top:"160px"});
+                            }else{
+                                //处于动画状态的话即蒙层处于最高处，此时让蒙层执行动画回到原处
+                                $(this).stop().animate({top:"160px"});
+                            }
+                        }
+                        //组织默认行为
+                        event.preventDefault();
+                    })
+                });
+            }
+        }
+        urlredirect();
+
+
 
 
 
